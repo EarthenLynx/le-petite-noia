99a96356f67631d2b09712107ab5345318977558
EarthenLynx <t.quante@gmx.net>
2020-09-06 14:47:58 +0200

    Added toast

---
+++ src/fragments/Toast.vue
@@ -0,0 +1,22 @@
+<template>
+	<div class="toast">
+		{{ msg }}
+	</div>
+</template>
+
+<script>
+export default {
+	props: {
+		msg: String,
+	},
+};
+</script>
+
+<style scoped>
+.toast {
+  position: absolute; 
+  top: 5%; 
+  right: 5%;
+  width: 350px;
+}
+</style>
